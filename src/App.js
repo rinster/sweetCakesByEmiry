901444ca2f04aafd2b53762a5213d91e239fa55f
Erine Natnat <enatnat.08@gmail.com>
2019-01-26 21:10:49 -0800

    created landing page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react';
 import './resources/css/styles.css';
 import './resources/css/daFont.css';
 
+import LandingPage from './components/LandingPage/landingPage';
+
 class App extends Component {
   render() {
     return (
-      <div className="App" 
-          style={{
-            fontFamily:"AutumnInNovember",
-            fontSize: "100px",
-            backgroundImage: "linear-gradient(to right, #E493CB, #6774DE)",
-            textAlign: "center",
-            webkitBackgroundClip: "text",
-            color: "transparent",
-          }}>
-        Hello World
+      <div className="App">
+        <LandingPage/>
       </div>
     );
   }
---
+++ src/components/LandingPage/landingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <header className="header">
+
+            <div className="header__text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary--main">Sweet Cakes</span>
+                    <span className="heading-primary--sub">by Emiry</span>
+                </h1>
+            </div>
+            
+        </header>
+    );
+};
+
+export default LandingPage;
