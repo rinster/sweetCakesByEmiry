0f02652bdb5b8bfd31ddcc8b97843b183c4f76db
Erine Natnat <enatnat.08@gmail.com>
2019-01-26 18:09:09 -0800

    added custom font test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './resources/css/styles.css';
+import './resources/css/daFont.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" 
+          style={{
+            fontFamily:"AutumnInNovember",
+            fontSize: "100px",
+            backgroundImage: "linear-gradient(to right, #E493CB, #6774DE)",
+            textAlign: "center",
+            webkitBackgroundClip: "text",
+            color: "transparent",
+          }}>
         Hello World
       </div>
     );
